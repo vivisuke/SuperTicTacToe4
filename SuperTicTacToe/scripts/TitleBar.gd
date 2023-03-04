a10ea301365610e52fd7d0fc856fca302f401421
vivisuke <ntsuda@master.email.ne.jp>
2023-03-05 08:38:51 +0900

    0003 ◎ C 12/27 03/05 0.001│ToDo│タイトルバー：描画スクリプトアタッチ、角丸・縁取り・影

---
+++ SuperTicTacToe/scripts/TitleBar.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+const RADIUS = 5
+
+func _draw():
+	var style_box = StyleBoxFlat.new()      # 影、ボーダなどを描画するための矩形スタイルオブジェクト
+	style_box.bg_color = color   # 矩形背景色
+	style_box.border_color = Color.GREEN
+	style_box.set_border_width_all(2)
+	style_box.set_corner_radius_all(RADIUS)
+	style_box.shadow_offset = Vector2(0, 4)     # 影オフセット
+	style_box.shadow_size = 8                   # 影（ぼかし）サイズ
+	draw_style_box(style_box, Rect2(Vector2(0, 0), self.size))      # style_box に設定した矩形を描画
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
