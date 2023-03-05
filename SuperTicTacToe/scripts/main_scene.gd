a83d46d33b3806e2ce745c345728ad090d12eaea
vivisuke <ntsuda@master.email.ne.jp>
2023-03-05 17:00:54 +0900

    0009 ◎ C 12/27 03/05 0.001│ToDo│ローカルボード強調用タイルマップ（TileMapBG）追加 0010 ◎ C 12/27 03/05 0.001│ToDo│ローカルボード用タイルマップ（TileMapLocal）追加

--- SuperTicTacToe/scripts/main_scene.gd
+++ SuperTicTacToe/scripts/main_scene.gd
@@ -3,8 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	printraw("_ready \n")
+	#printraw("_ready \n")
 	print("OK")
+	#$Board/TileMapLocal.erase_cell(0, Vector2i(3, 0))
+	$Board/TileMapLocal.set_cell(0, Vector2i(3, 0), 1, Vector2i(0, 0))
+	print("(0, 0) = ", $Board/TileMapLocal.get_cell_source_id (0, Vector2i(0, 0)))
+	$Board/TileMapLocal.set_cell(0, Vector2i(0, 0), 1)
+	print("(0, 0) = ", $Board/TileMapLocal.get_cell_source_id (0, Vector2i(0, 0)))
+	print("(3, 0) = ", $Board/TileMapLocal.get_cell_source_id (0, Vector2i(3, 0)))
+	print("(4, 1) = ", $Board/TileMapLocal.get_cell_source_id (0, Vector2i(4, 1)))
 	pass # Replace with function body.
 
 
@@ -15,4 +22,8 @@ func _process(delta):
 
 func _on_start_stop_button_pressed():
 	print("_on_start_stop_button_pressed()\n")
+	print("(0, 0) = ", $Board/TileMapBG.get_cell_source_id (0, Vector2i(0, 0)))
+	$Board/TileMapBG.set_cell(0, Vector2i(0, 0), 0)
+	print("(0, 0) = ", $Board/TileMapBG.get_cell_source_id (0, Vector2i(0, 0)))
+	print("(1, 1) = ", $Board/TileMapBG.get_cell_source_id (0, Vector2i(1, 1)))
 	pass # Replace with function body.
