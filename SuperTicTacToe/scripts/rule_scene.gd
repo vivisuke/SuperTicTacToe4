0378fc87a9a4ba30cf438c2315df105e72738ae8
vivisuke <ntsuda@master.email.ne.jp>
2023-03-30 06:59:46 +0900

    0097 ◎ C 03/30 03/30 0.001│ToDo│最初・最後のページでは前・後ボタンをディセーブル

--- SuperTicTacToe/scripts/rule_scene.gd
+++ SuperTicTacToe/scripts/rule_scene.gd
@@ -4,9 +4,10 @@ var page = 0;			#
 var pict = []
 
 var rule_text = [
-	"Super Tic-Tac-Toe（超三目並べ）は、小さい盤面（ローカルボード）に○☓ を交互に打っていき、大きい盤面（グローバルボード）で先に三目並べた方が勝ちのゲームです。",
-	"ローカルボードで○（☓）三目並びを作ると、グローバルボードの対応するセルに大きい○（☓）が置かれます。",
-	"ローカルボード内に打った箇所により、次に着手可能なローカルボード（背景が白）が決まります。",
+	"Super Tic-Tac-Toe（超三目並べ）は, 小さい盤面（ローカルボード）に○☓ を交互に打っていき, 大きい盤面（グローバルボード）で先に三目並びを作った方が勝ちのゲームです。",
+	"ローカルボードで○（☓）三目並びを作ると, グローバルボードの対応するセルに大きい○（☓）が置かれます。",
+	"ローカルボード内に打った箇所により, 次に着手可能なローカルボード（背景が白）が決まります。" +
+	"ただし次がすでに三目並んでいる, または空きが無い場合は, 全ての箇所に打つことが可能です。",
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -25,6 +26,8 @@ func update_pict_text_buttons():
 			pict[i].hide()
 		#pict[i].visible = i == page
 	$RuleLabel.text = rule_text[page]
+	$PrevButton.disabled = page == 0
+	$NextButton.disabled = page == rule_text.size() - 1
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
