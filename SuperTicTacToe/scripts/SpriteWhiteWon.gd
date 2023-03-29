3b5e2772ceb72ee5b641858fc3cff1c17c3db2f5
vivisuke <ntsuda@master.email.ne.jp>
2023-03-30 05:58:24 +0900

    0089 ◎ C 03/28 03/30 0.001│ToDo│ルールシーン：左右ボタン設置 0090 ◎ C 03/28 03/30 0.001│ToDo│ルールシーン：説明ラベル設置 0091 ◎ C 03/28 03/30 0.001│ToDo│ローカルボード説明画像作成

---
+++ SuperTicTacToe/scripts/SpriteWhiteWon.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _draw():
+	var style_box = StyleBoxFlat.new()      # 影、ボーダなどを描画するための矩形スタイルオブジェクト
+	style_box.bg_color = Color.TRANSPARENT   # 矩形背景色
+	style_box.shadow_offset = Vector2(0, 4)     # 影オフセット
+	style_box.shadow_size = 8                   # 影（ぼかし）サイズ
+	draw_style_box(style_box, Rect2(Vector2(-250, -250), Vector2(500, 500)))      # style_box に設定した矩形を描画
+	pass
--- SuperTicTacToe/scripts/rule_scene.gd
+++ SuperTicTacToe/scripts/rule_scene.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
 
+var rule_text = [
+	"Super Tic-Tac-Toe（超三目並べ）は、小さい盤面（ローカルボード）に○☓ を交互に打っていき、大きい盤面（グローバルボード）で先に三目並べた方が勝ちのゲームです。",
+	"ローカルボードで○（☓）三目並びを作ると、グローバルボードの対応するセルに大きい○（☓）が置かれます。",
+	"ローカルボード内に打った箇所により、次に着手可能なローカルボード（背景が白）が決まります。",
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$RuleLabel.text = rule_text[2]
 	pass # Replace with function body.
 
 
